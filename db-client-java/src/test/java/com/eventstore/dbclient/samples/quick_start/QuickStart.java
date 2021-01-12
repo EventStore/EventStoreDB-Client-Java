3af1e9586113ed1264821fb83001bc2898c3919c
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-01-12 10:59:06 +0100

    Added documentation snippets for reading events

--- db-client-java/src/test/java/com/eventstore/dbclient/samples/quick_start/QuickStart.java
+++ db-client-java/src/test/java/com/eventstore/dbclient/samples/quick_start/QuickStart.java
@@ -1,16 +1,18 @@
-package com.eventstore.dbclient.samples;
+package com.eventstore.dbclient.samples.quick_start;
 
 import com.eventstore.dbclient.*;
+import com.eventstore.dbclient.samples.TestEvent;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-public class GetStarted {
+public class QuickStart {
     public static void Run() throws ParseError, ExecutionException, InterruptedException {
         // region createClient
         EventStoreDBClientSettings settings = EventStoreDBConnectionString.parse("{connectionString}");
         EventStoreDBClient client = EventStoreDBClient.create(settings);
-        // region createClient
+        // endregion createClient
 
         // region createEvent
         TestEvent event = new TestEvent();
@@ -23,16 +25,28 @@ public class GetStarted {
         // endregion createEvent
 
         // region appendEvents
-        WriteResult writeResult = client.appendToStream("some-stream", eventData)
+        client.appendToStream("some-stream", eventData)
                 .get();
         // endregion appendEvents
 
         // region overriding-user-credentials
-        AppendToStreamOptions options = AppendToStreamOptions.get()
+        AppendToStreamOptions appendToStreamOptions = AppendToStreamOptions.get()
                 .authenticated(new UserCredentials("admin", "changeit"));
 
-        client.appendToStream("some-stream", options, eventData)
+        client.appendToStream("some-stream", appendToStreamOptions, eventData)
                 .get();
         // endregion overriding-user-credentials
+
+
+        // region readStream
+        ReadStreamOptions options = ReadStreamOptions.get()
+                .forwards()
+                .fromStart();
+
+        ReadResult result = client.readStream("some-stream", 10, options)
+                .get();
+
+        List<ResolvedEvent> events = result.getEvents();
+        // endregion readStream
     }
 }
---
+++ db-client-java/src/test/java/com/eventstore/dbclient/samples/reading_events/ReadingEvents.java
@@ -0,0 +1,220 @@
+package com.eventstore.dbclient.samples.reading_events;
+
+import com.eventstore.dbclient.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class ReadingEvents {
+    private static void readFromStream(EventStoreDBClient client) throws ExecutionException, InterruptedException, JsonProcessingException {
+        // region read-from-stream
+        ReadStreamOptions options = ReadStreamOptions.get()
+                .forwards()
+                .fromStart();
+
+        ReadResult result = client.readStream("some-stream", options)
+                .get();
+
+        List<ResolvedEvent> events = result.getEvents();
+        // endregion read-from-stream
+
+        // region iterate-stream
+        for (ResolvedEvent resolvedEvent : events) {
+            RecordedEvent recordedEvent = resolvedEvent.getOriginalEvent();
+            System.out.println(new ObjectMapper().writeValueAsString(recordedEvent.getEventData()));
+        }
+        // endregion iterate-stream
+    }
+
+    private static void readFromStreamPosition(EventStoreDBClient client) throws ExecutionException, InterruptedException, JsonProcessingException {
+        // region read-from-stream-position
+        ReadStreamOptions options = ReadStreamOptions.get()
+                .forwards()
+                .fromRevision(10);
+
+        ReadResult result = client.readStream("some-stream", 20, options)
+                .get();
+
+        List<ResolvedEvent> events = result.getEvents();
+        // endregion read-from-stream-position
+
+        // region iterate-stream
+        for (ResolvedEvent resolvedEvent : events) {
+            RecordedEvent recordedEvent = resolvedEvent.getOriginalEvent();
+            System.out.println(new ObjectMapper().writeValueAsString(recordedEvent.getEventData()));
+        }
+        // endregion iterate-stream
+    }
+
+    private static void readStreamOverridingUserCredentials(EventStoreDBClient client) throws ExecutionException, InterruptedException {
+        // region overriding-user-credentials
+        UserCredentials credentials = new UserCredentials("admin", "changeit");
+
+        ReadStreamOptions options = ReadStreamOptions.get()
+                .forwards()
+                .fromStart()
+                .authenticated(credentials);
+
+        ReadResult result = client.readStream("some-stream", options)
+                .get();
+        // endregion overriding-user-credentials
+    }
+
+    private static void readFromStreamPositionCheck(EventStoreDBClient client) throws JsonProcessingException, InterruptedException {
+        // region checking-for-stream-presence
+        ReadStreamOptions options = ReadStreamOptions.get()
+                .forwards()
+                .fromRevision(10);
+
+        List<ResolvedEvent> events = null;
+        try {
+            ReadResult result = client.readStream("some-stream", 20, options)
+                    .get();
+            events = result.getEvents();
+        } catch (ExecutionException e) {
+            Throwable innerException = e.getCause();
+
+            if (innerException instanceof StreamNotFoundException) {
+                return;
+            }
+        }
+
+        for (ResolvedEvent resolvedEvent : events) {
+            RecordedEvent recordedEvent = resolvedEvent.getOriginalEvent();
+            System.out.println(new ObjectMapper().writeValueAsString(recordedEvent.getEventData()));
+        }
+        // endregion checking-for-stream-presence
+    }
+
+    private static void readFromStreamBackwards(EventStoreDBClient client) throws JsonProcessingException, ExecutionException, InterruptedException {
+        // region reading-backwards
+        ReadStreamOptions options = ReadStreamOptions.get()
+                .backwards()
+                .fromEnd();
+
+        ReadResult result = client.readStream("some-stream", options)
+                .get();
+
+        List<ResolvedEvent> events = result.getEvents();
+
+        for (ResolvedEvent resolvedEvent : events) {
+            RecordedEvent recordedEvent = resolvedEvent.getOriginalEvent();
+            System.out.println(new ObjectMapper().writeValueAsString(recordedEvent.getEventData()));
+        }
+        // endregion reading-backwards
+    }
+
+    private static void readFromAllStream(EventStoreDBClient client) throws JsonProcessingException, ExecutionException, InterruptedException {
+        // region read-from-all-stream
+        ReadAllOptions options = ReadAllOptions.get()
+                .forwards()
+                .fromStart();
+
+        ReadResult result = client.readAll(options)
+                .get();
+
+        List<ResolvedEvent> events = result.getEvents();
+        // endregion read-from-all-stream
+
+        // region read-from-all-stream-iterate
+        for (ResolvedEvent resolvedEvent : events) {
+            RecordedEvent recordedEvent = resolvedEvent.getOriginalEvent();
+            System.out.println(new ObjectMapper().writeValueAsString(recordedEvent.getEventData()));
+        }
+        // endregion read-from-all-stream-iterate
+    }
+
+    private static void readAllOverridingUserCredentials(EventStoreDBClient client) throws ExecutionException, InterruptedException {
+        // region read-all-overriding-user-credentials
+        UserCredentials credentials = new UserCredentials("admin", "changeit");
+
+        ReadAllOptions options = ReadAllOptions.get()
+                .forwards()
+                .fromStart()
+                .authenticated(credentials);
+
+        ReadResult result = client.readAll(options)
+                .get();
+        // endregion read-all-overriding-user-credentials
+    }
+
+    private static void ignoreSystemEvents(EventStoreDBClient client) throws JsonProcessingException, ExecutionException, InterruptedException {
+        // region ignore-system-events
+        ReadAllOptions options = ReadAllOptions.get()
+                .forwards()
+                .fromStart();
+
+        ReadResult result = client.readAll(options)
+                .get();
+
+        List<ResolvedEvent> events = result.getEvents();
+
+        for (ResolvedEvent resolvedEvent : events) {
+            RecordedEvent recordedEvent = resolvedEvent.getOriginalEvent();
+            if (recordedEvent.getEventType().startsWith("$")) {
+                continue;
+            }
+            System.out.println(new ObjectMapper().writeValueAsString(recordedEvent.getEventData()));
+        }
+        // endregion ignore-system-events
+    }
+
+    private static void readFromAllStreamBackwards(EventStoreDBClient client) throws JsonProcessingException, ExecutionException, InterruptedException {
+        // region read-from-all-stream-backwards
+        ReadAllOptions options = ReadAllOptions.get()
+                .backwards()
+                .fromEnd();
+
+        ReadResult result = client.readAll(options)
+                .get();
+
+        List<ResolvedEvent> events = result.getEvents();
+        // endregion read-from-all-stream-backwards
+
+        // region read-from-all-stream-iterate
+        for (ResolvedEvent resolvedEvent : events) {
+            RecordedEvent recordedEvent = resolvedEvent.getOriginalEvent();
+            System.out.println(new ObjectMapper().writeValueAsString(recordedEvent.getEventData()));
+        }
+        // endregion read-from-all-stream-iterate
+    }
+
+    private static void filteringOutSystemEvents(EventStoreDBClient client) throws JsonProcessingException, ExecutionException, InterruptedException {
+        ReadAllOptions options = ReadAllOptions.get()
+                .forwards()
+                .fromStart();
+
+        ReadResult result = client.readAll(options)
+                .get();
+
+        List<ResolvedEvent> events = result.getEvents();
+
+        for (ResolvedEvent resolvedEvent : events) {
+            RecordedEvent recordedEvent = resolvedEvent.getOriginalEvent();
+            if (!recordedEvent.getEventType().startsWith("$")) {
+                continue;
+            }
+            System.out.println(new ObjectMapper().writeValueAsString(recordedEvent.getEventData()));
+        }
+    }
+
+    private static void readFromStreamResolvingLinkTos(EventStoreDBClient client) throws JsonProcessingException, ExecutionException, InterruptedException {
+        // region read-from-all-stream-resolving-link-Tos
+        ReadAllOptions options = ReadAllOptions.get()
+                .forwards()
+                .fromStart()
+                .resolveLinkTos();
+
+        ReadResult result = client.readAll(options)
+                .get();
+
+        List<ResolvedEvent> events = result.getEvents();
+        // endregion read-from-all-stream-resolving-link-Tos
+        for (ResolvedEvent resolvedEvent : events) {
+            RecordedEvent recordedEvent = resolvedEvent.getOriginalEvent();
+            System.out.println(new ObjectMapper().writeValueAsString(recordedEvent.getEventData()));
+        }
+    }
+}
